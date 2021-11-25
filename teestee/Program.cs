69874a43db336ed6477fc21d15cd12b8514b82a9
Vinicius Lopes <ikdonsh@hotmail.com>
2021-11-25 12:37:46 -0300

    Acrecentada a pergunta Como voce esta hoje

--- teestee/Program.cs
+++ teestee/Program.cs
@@ -8,6 +8,7 @@ namespace teestee
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia");
+            Console.WriteLine("Como você está hoje? ");
         }
     }
 }

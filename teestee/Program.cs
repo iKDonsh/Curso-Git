22101e6b8d5af69de686375b0fd4195f165d7c8f
Vinicius Lopes <ikdonsh@hotmail.com>
2021-11-25 11:54:34 -0300

    Projeto iniciado mostrando a mensagem Ola mundo

---
+++ teestee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace teestee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
